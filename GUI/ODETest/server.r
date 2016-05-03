34f87b1a4e86a73c130334c1c0f46344ce90e3a0
chipdelmal <chipdelmal@gmail.com>
2016-05-02 17:26:16 -0700

    Testing event-handled plots
    Ugly implementation of event-handled plots… there’s something really
    misterious about how Shiny handles events.

--- GUI/ODETest/server.r
+++ GUI/ODETest/server.r
@@ -8,11 +8,31 @@ shinyServer(function(input,output,session){
     NumMosq
   })
   output$recalculate = eventReactive(input$runModel,{
-    IVM_traj = data.frame(dede(y=initState,times=times,parms=theta,func=IVM_ode,method="lsoda"))
-    print(IVM_traj)
+    IVM_traj <<- data.frame(dede(
+      y=initState,
+      times=seq(0,input$maxTime),
+      parms=theta,
+      func=IVM_ode,
+      method="lsoda"
+    ))
+    #print(IVM_traj)
+    NumMosq <<- c(
+      sum(IVM_traj[2]),
+      sum(IVM_traj[3]),
+      sum(IVM_traj[4]),
+      sum(IVM_traj[5]),
+      sum(IVM_traj[6]),
+      sum(IVM_traj[7])
+    )
+    #print(NumMosq)
   })
-  output$replot = eventReactive(input$plot,{
-    1
+  output$barChart = eventReactive(input$plot,{
+    output$barChart = renderPlot({barplot(NumMosq , 
+      main='An. Gambiae ITN & IRS & Cattle = 0.8', 
+      xlab='Time (days)',
+      ylab='Number of Mosquitoes',
+      names.arg=c('EL','LL','PL','SV','EV','IV'))
+    })
   })
 })
 ###############################################################################
--- GUI/ODETest/ui.r
+++ GUI/ODETest/ui.r
@@ -11,15 +11,19 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       headerPanel(h1("Inputs")),
-      actionButton("testButton","TESTING",width="100%"),
+      #actionButton("testButton","TESTING",width="100%"),
       actionButton("runModel","RUN",width="100%"),
-      actionButton("plot","PLOT",width="100%")
+      actionButton("plot","PLOT",width="100%"),
+      sliderInput("maxTime","Number of days:",min=1,max=365,value=50)
     ),
     mainPanel(
-      headerPanel(h1("Outputs")),
-      textOutput("reactive"),
-      textOutput("recalculate"),
-      textOutput("replot")
+      fluidRow(
+        headerPanel(h1("Outputs")),
+        textOutput("reactive"),
+        textOutput("recalculate"),
+        #plotOutput("replot"),
+        plotOutput("barChart")
+      )
     )
   )
   #############################################################################
