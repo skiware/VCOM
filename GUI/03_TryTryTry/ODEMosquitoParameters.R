12ae1c3ae7711ad3510ed4f1f96be858c8b7d6c0
chipdelmal <chipdelmal@gmail.com>
2016-05-04 10:43:19 -0700

    Parameters Documentation Routine
    Created a Mathematica notebook that pattern-matches all the comments
    starting with “#*” and automatically generates a table with the
    description given in the code for them.

--- GUI/03_TryTryTry/ODEMosquitoParameters.R
+++ GUI/03_TryTryTry/ODEMosquitoParameters.R
@@ -38,38 +38,22 @@ theta <- c(
 
 getTheta = function(lifeCycleParameters, interventionParameters){
   c(
-    ## Mosquito life cycle parameters:
-    beta = 21.19, # Number of eggs laid per day by female mosquito
-    muEL = 0.034, # Early larval instar daily mortality
-    muLL = 0.035, # Late larval instar daily mortality
-    muPL = 0.25, # Pupal daily mortality
-    durEL = 6.64, # Duration of early instar stage
-    durLL = 3.72, # Duration of late instar stage
-    durPL = 0.64, # Duration of pupal stage
-    durEV = 10, # Duration of latent period in mosquito (days)
-    gamma = 13.25, # Effect of density-dependence on late instars relative to early instars
-    tau1 = 0.68, # Time spent foraging for a blood meal at 0% ITN coverage
-    tau2 = 2.32, # Time spent resting and ovipositing by a mosquito
-    ## Additional transmission parameters:
-    f0 = 1/3, # Daily biting rate by mosquitoes on animals and humans
-    epsilon0 = 10/365, # Daily entomological inolculation rate
-    iH_eq = 0.35, # Equilibrium malaria prevalence in humans
-    NH_eq = 200, # Equilibrium human population size
-    bV = 0.05 # Probability of transmission from human to vector per infectious bite
+    
   )
 }
 
+######################################################################################
 #------------------------------------------------------------------------
 ############ SPECIES SPECIFIC PARAMETERS ################################
 #------------------------------------------------------------------------
-# muV   Adult mosquito daily mortality
-# Q0    Human blood index
-# phiB  Proportion of bites on a person while they are in bed
-# phiI  Proportion of bites on a person while they are indoors
-# rITN  Probability of mosquito repeating a feeding attempt due to IRS
-# sITN  Probability of mosquito feeding and surviving in presence of ITN
-# rIRS  Probability of mosquito repeating a feeding attempt due to IRS
-# sIRS  Probability of mosquito feeding and surviving in presence of IRS
+#* muV     Adult mosquito daily mortality
+#* Q0      Human blood index
+#* phiB    Proportion of bites on a person while they are in bed
+#* phiI    Proportion of bites on a person while they are indoors
+#* rITN    Probability of mosquito repeating a feeding attempt due to IRS
+#* sITN    Probability of mosquito feeding and surviving in presence of ITN
+#* rIRS    Probability of mosquito repeating a feeding attempt due to IRS
+#* sIRS    Probability of mosquito feeding and surviving in presence of IRS
 #------------------------------------------------------------------------
 getAnGambiaeParameters = function(){
   c(
@@ -95,18 +79,16 @@ getAnFunestusParameters = function(){
     rIRS = 0.63, sIRS = 0
   )
 }
-#########################################################################
-
-
+######################################################################################
 #------------------------------------------------------------------------
 ############ INTERVENTIONS PARAMETERS ###################################
 #------------------------------------------------------------------------
-# ITNcov        ITN coverage
-# IRScov        IRS coverage
-# time_ITN_on   When ITNs are applied (days)
-# time_IRS_on   When IRSs are applied (days)
+#* ITNcov        ITN coverage
+#* IRScov        IRS coverage
+#* time_ITN_on   When ITNs are applied (days)
+#* time_IRS_on   When IRSs are applied (days)
 #------------------------------------------------------------------------
-getAnGambiaeParameters = function(){
+getInterventionsParameters = function(){
   c(
     ITNcov = 0.8, IRScov = 0.8,
     time_ITN_on = 50, time_IRS_on = 50
@@ -114,3 +96,50 @@ getAnGambiaeParameters = function(){
 }
 #########################################################################
 #------------------------------------------------------------------------
+######################################################################################
+#------------------------------------------------------------------------
+############ MOSQUITO LIFE CYCLE PARAMETERS #############################
+#------------------------------------------------------------------------
+#* beta    Number of eggs laid per day per female mosquito
+#* muEL    Early larval instar daily mortality
+#* muLL    Late larvarl instar daily mortality
+#* muPL    Pupal daily mortality
+#* durEL   Duration of early instar stage
+#* durLL   Duration of late instar stage
+#* durPL   Pupal daily mortality
+#* durPL   Duration of pupal stage
+#* durEV   Duration of latent period in mosquito (days)
+#* gamma   Effect of density-dependence on late instars relative to early instars
+#* tau1    Time spent foraginf for a blood meal at 0% ITN coverage
+#* tau2    Time spent resting and ovipositing by a mosquito
+#------------------------------------------------------------------------
+getLifeCycleParameters = function(){
+  c(
+    beta = 21.19,
+    muEL = 0.034, muLL = 0.035, muPL = 0.25,
+    durEL = 6.64, durLL = 3.72, durPL = 0.64, durEV = 10,
+    gamma = 13.25, tau1 = 0.68, tau2 = 2.32
+  )
+}
+#########################################################################
+#------------------------------------------------------------------------
+######################################################################################
+#------------------------------------------------------------------------
+############ ADDITIONAL TRANSMISSION PARAMETERS #########################
+#------------------------------------------------------------------------
+#* f0        Daily biting rate by mosquitoes on animals and humans
+#* epsilon0  Daily entomological inolculation rate
+#* iH_eq     Equilibrium malaria prevalence in humans
+#* NH_eq     Equilibrium human population size
+#* bV        Probability of transmission from human to vector per infectious bite
+#------------------------------------------------------------------------
+getAnGambiaeParameters = function(){
+  c(
+    f0 = 1/3, epsilon0 = 10/365, 
+    iH_eq = 0.35, NH_eq = 200, 
+    bV = 0.05
+  )
+}
+#########################################################################
+#------------------------------------------------------------------------
+######################################################################################
