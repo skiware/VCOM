c17e193ad754a1a2ea5e39a57d2b39f65e7549f9
skiware <sskiware@gmail.com>
2016-05-19 16:12:45 -0700

    Initialize
    Initialize control measures using the driver

--- GUI/06_VCOM/VCOM-driver.R
+++ GUI/06_VCOM/VCOM-driver.R
@@ -22,7 +22,22 @@ source("ODEAuxiliaryFunctions.R")
 source("ODEControlMeasuresParameters.R")
 source("ODETransmissionParameters.R")
 ########### INIT MODEL ##################################################
-theta = getTheta()
+inter=  c(
+  OVIcov = 0.0, time_OVI_on = 00,
+  FOGcov = 0.0, time_FOG_on = 00,
+  LARcov = 0.0, time_LAR_on = 00,
+  BIOcov = 0.0, time_BIO_on = 00,
+  SREcov = 0.0, time_SRE_on = 00,
+  IRScov = 1, time_IRS_on = 10,
+  ITNcov = 1, time_ITN_on = 10,
+  IVMcov = 0.0, time_IVM_on = 00,
+  HOUcov = 0.0, time_HOU_on = 00,
+  ODOcov = 0.0, time_ODO_on = 00,
+  SPAcov = 0.0, time_SPA_on = 00
+  
+  #expand.grid(c(0,1),c(0,1),c(0,1),c(0,1),c(0,1))
+)
+theta = getTheta(interventionParameters=inter)
 initState = calculateInitialState(theta) 
 ########### RUN THE IVM ODEs ############################################
 tMax <- 360 # Simulation runs up to 365 days
