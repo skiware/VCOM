f0739c7ddc399e6ec008227333ea20b0f54fcadf
skiware <sskiware@gmail.com>
2016-07-12 15:54:39 -0700

    Model Output
    Added a function to compute model output 1) human biting rate 2)
    mosquito density 3) Human blood index 4) Vectoral capacity, 4) EIR, and
    5) Reproduction Number R0

---
+++ GUI/09_VCOM/SIMPLE/ODEModelOutput.R
@@ -0,0 +1,64 @@
+########################################################################
+# ODEModelOutput.R
+# Contains the functions that produce model outputs
+#
+#=======================================================================
+########################################################################
+
+######################################################################################
+produceHumanBitingRate = function(V,H){
+  #. produceHumanBitingRate: Main function that return human biting rate per mosquito in presence of intervetion
+  
+  Mdensity <- V/H
+  
+  return(Mdensity)
+}
+######################################################################################
+produceMosqDensity = function(V,H){
+  #. produceMosqDensity: Main function that return mosquito density in presence of intervetion
+  
+  Mdensity <- V/H
+  
+  return(Mdensity)
+}
+######################################################################################
+produceHBI = function(wCom_human,wCom){
+  #. produceHBI: Main function that return human blood index
+  
+  #HBI_com <- (Pfi+pfo)/pf
+  HBI_com <- (wCom_human)/wCom
+  
+  return(HBI_com)
+}
+
+######################################################################################
+produceVC = function(a_theta, Mdensity,muVCom,durEV){
+  #. produceVC: Main function that return vectorial capacity
+  
+  ## n incubation period
+  tilda <- durEV
+  
+  VC <- (Mdensity * a_theta^2 *exp(-muVCom*tilda))/muVCom
+  
+  return(VC)
+}
+######################################################################################
+produceEIR = function(a_theta, IV, H){
+  #. produceEIR: Main function that return entomological innoculation rate
+  
+  ## X_theta = iH_eq, bh - add to transmission parameter
+  
+  
+  EIR <- (IV*a_theta)/H
+  
+  return(EIR)
+}
+
+######################################################################################
+produceEIR = function(a_theta,muVCom, V,bv,bh,H){
+  #. produceR_0: Main function that return entomological innoculation rate
+  
+  R_o <- (V/H)*a_theta^2*bv*(bh/(muVCom*V))
+  
+  return(R_o)
+}
