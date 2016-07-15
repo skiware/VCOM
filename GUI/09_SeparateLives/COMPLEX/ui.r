b141e851b84451fafc1c6cf3bd9b602b59f3caa8
chipdelmal <chipdelmal@gmail.com>
2016-07-15 16:16:56 -0700

    Complex GUI error message extended
    The complex GUI now displays a meaningful message when uploading an
    error file. Before this it displayed a large table with garbage.
    
    THOSE DAMNED LINES AT THE TOP ARE STILL THERE!!!! I JUST CAN’T FIND A
    WAY TO GET RID OF THEM!!!! IT’S DRIVING ME INSANE!!!!!!!

--- GUI/09_SeparateLives/COMPLEX/ui.r
+++ GUI/09_SeparateLives/COMPLEX/ui.r
@@ -21,6 +21,9 @@ source("ODEAuxiliaryFunctions.R")
 source("ODEControlMeasuresParameters.R")
 source("ODETransmissionParameters.R")
 ################################################################################
+DISALLOWED_HEADS<<-c("Life Cycle Parameters","Species-Specific Parameters","Interventions Parameters","Transmission Parameters",
+                     "NA.1","NA.2","NA.3","NA.4","NA.5","NA.6","NA.7","NA.8","NA.9","NA.10","NA.11","NA.12","NA.13","NA.14","NA.15",
+                     "NA.16","NA.17","NA.18","NA.19","NA.20","NA")
 importedFile=NULL
 BOXES_WIDTH<<-"75px"
 NAS_ALLOWED<<-11
