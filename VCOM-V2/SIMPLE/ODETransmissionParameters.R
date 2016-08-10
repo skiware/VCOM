e729698b3a532ef427c8202ed2d3ab8624b74a89
skiware <sskiware@gmail.com>
2016-08-10 13:12:54 -0700

    Created VCOM-V2
    To avoid corrupting changes made by Hector while I was offline and
    loosing the changes I made but didn't commit I have created VCOM-V2 with
    my latest codes

---
+++ VCOM-V2/SIMPLE/ODETransmissionParameters.R
@@ -0,0 +1,33 @@
+########################################################################
+#=======================================================================
+# ODETransmissionParameters.R
+# Contains the functions that deal with the transmission of a disease
+#
+#=======================================================================
+########################################################################
+
+######################################################################################
+#------------------------------------------------------------------------
+############ ADDITIONAL TRANSMISSION PARAMETERS #########################
+#------------------------------------------------------------------------
+#* f0:        Daily biting rate by mosquitoes on animals and humans
+#* epsilon0:  Daily entomological inolculation rate
+#* iH_eq:     Equilibrium malaria prevalence in humans
+#* NH_eq:     Equilibrium human population size
+#* bV:        Probability of transmission from human to vector per infectious bite
+#* bh:        transmission efficiency from an infectious mosquito to an uninfected, susceptible human
+#------------------------------------------------------------------------
+getAdditionalTransmissionParameters = function(
+  #For getting low, medium
+  f0=1/3,epsilon0=9/365,iH_eq=0.35,
+  NH_eq=200,bV=0.05,bh=0.5   #bv = originally 0.05
+){
+  #. getAdditionalTransmissionParameters: Returns the transmission parameters
+  c(
+    f0=f0,epsilon0=epsilon0, 
+    iH_eq=iH_eq,NH_eq=NH_eq, 
+    bV=bV,bh=bh
+  )
+}
+######################################################################################
+
---
+++ VCOM-V2/SIMPLE/server.r
@@ -0,0 +1,116 @@
+#------------------------------------------------------------------------------##
+#################################################################################
+## Hector M. Sanchez C. (sanchez.hmsc@itesm.mx)                               ###
+## Malaria vector ODE model GUI                                               ###
+## 02/May/2016                                                                ###
+#################################################################################
+#------------------------------------------------------------------------------##
+
+#################################################################################
+#---# MAIN SHINY SERVER APPLICATION #############################################
+shinyServer(
+  function(input,output,session){
+    #############################################################################
+    # PRIMING GUI ###############################################################
+    output$modelStatus <- renderText({"Waiting"})
+    shinyjs::disable("downloadCSVTrace"); shinyjs::disable("downloadPlotTrace")
+    shinyjs::disable("downloadCSVEIR"); shinyjs::disable("downloadPlotEIR")
+    shinyjs::disable("downloadCSVDemographics"); shinyjs::disable("downloadPlotDemographics")
+    output$plotTrajectory=renderPlot({plotTrajectory(IVM_traj)})
+    output$IVM_Runtime=renderTable(IVM_traj)
+    output$plotDemographics = renderPlot({barChartMosquitoDemographics(IVM_traj)})
+    output$plotEIR = renderPlot({plotEIR(IVM_traj,theta,timing)})
+    #############################################################################
+    # CLICK EVENTS ##############################################################
+    observeEvent(input$buttonTest,{cat("Button event!\n")})
+    observeEvent(input$radioSpecies,{cat("Radio event!\n")})
+    observeEvent(input$sliderTime,{cat("Slider event!\n")})
+    #############################################################################
+    # INTERVENTIONS_EVENTS ######################################################
+    #observeEvent(input$OVIcov|input$FOGcov|input$LARcov|input$BIOcov|input$SREcov|input$ITNcov|input$IRScov|input$IVMcov|input$HOUcov|input$ODOcov|input$SPAcov,{
+    #    cat("Coverage Slider Event!\n")
+    #})
+    #############################################################################
+    # RUN MODEL #################################################################
+    observeEvent(input$buttonRun,{
+      output$modelStatus <- renderText({"Calculating. Please wait."})
+      cat("Run button event!\n")
+      # CALCULATE THETA ---------------------------------------------------------
+      MOSQUITO_PARAMETERS <<- switch(input$radioSpecies,
+        "GAM"=getAnGambiaeParameters(),
+        "ARA"=getAnArabiensisParameters(),
+        "FUN"=getAnFunestusParameters()
+      )
+      INTERVENTION_PARAMETERS <<- getInterventionsParameters(
+        OVIcov=input$OVIcov,FOGcov=input$FOGcov,LARcov=input$LARcov,BIOcov=input$BIOcov,SREcov=input$SREcov,ITNcov=input$ITNcov,
+        IRScov=input$IRScov,ECScov=input$ECScov,ECTcov=input$ECTcov,HOUcov=input$HOUcov,OBTcov=input$OBTcov,SPRcov=input$SPRcov,PPMcov=input$PPMcov,
+        time_OVI_on=input$time_OVI_on,time_FOG_on=input$time_FOG_on,time_LAR_on=input$time_LAR_on,time_BIO_on=input$time_BIO_on,
+        time_SRE_on=input$time_SRE_on,time_ITN_on=input$time_ITN_on,time_IRS_on=input$time_IRS_on,time_ECS_on=input$time_ECS_on,
+        time_ECT_on=input$time_ECT_on,time_HOU_on=input$time_HOU_on,time_OBT_on=input$time_OBT_on,time_SPR_on=input$time_SPR_on,
+        time_PPM_on=input$time_PPM_on
+      )
+      theta <<- getTheta(
+        speciesSpecificParameters=MOSQUITO_PARAMETERS,
+        interventionParameters=INTERVENTION_PARAMETERS
+      )
+      #--------------------------------------------------------------------------
+      initState=calculateInitialState(theta)
+      IVM_traj<<-runODE(input$sliderTime,1,initState,theta,"lsoda")
+      output$plotDemographics=renderPlot({barChartMosquitoDemographics(IVM_traj)})
+      output$plotTrajectory=renderPlot({plotTrajectory(IVM_traj)})
+      timing<<-seq(0,input$sliderTime,by=1)
+      output$plotEIR = renderPlot({plotEIR(IVM_traj,theta,timing)})
+      print(INTERVENTION_PARAMETERS)
+      #--------------------------------------------------------------------------
+      shinyjs::enable("downloadCSVTrace"); shinyjs::enable("downloadPlotTrace")
+      shinyjs::enable("downloadCSVEIR"); shinyjs::enable("downloadPlotEIR")
+      shinyjs::enable("downloadCSVDemographics"); shinyjs::enable("downloadPlotDemographics")
+      output$modelStatus <- renderText({"Done!"})
+    })
+    #############################################################################
+    # DOWNLOADS HANDLERS ########################################################
+    output$downloadParameters <- downloadHandler(
+      filename <- function(){paste("VCOM_Parameters","csv",sep=".")},
+      content <- function(file){
+        df=data.frame(theta)
+        df$Description=0
+        names(df)=c("Value","Description")
+        write.csv(df,file)
+      }
+    )
+    output$downloadCSVTrace <- downloadHandler(
+      filename <- function(){paste("VCOM_Trace","csv",sep=".")},
+      content <- function(file){
+        write.csv(IVM_traj,file)
+      }
+    )
+#     output$downloadCSVEIR <- downloadHandler(
+#       filename <- function(){paste("VCOM_EIR","csv",sep=".")},
+#       content <- function(file){
+#         write.csv(IVM_traj,file)
+#       }
+#     )
+    output$downloadPlotTrace <- downloadHandler(
+      filename = function(){paste(input$dataset,'TrajectoryPlot.png',sep='')},
+      content = function(file){
+        device <- function(...,width,height){grDevices::png(...,width=2*width,height=height,res=300,units="in")}
+        ggsave(file,plot=plotTrajectory(IVM_traj),device=device)
+      }
+    )
+    output$downloadPlotEIR <- downloadHandler(
+      filename = function(){paste(input$dataset,'EIRPlot.png',sep='')},
+      content = function(file){
+        device <- function(...,width,height){grDevices::png(...,width=2*width,height=height,res=300,units="in")}
+        ggsave(file,plot=plotEIR(IVM_traj,theta,time),device=device)
+      }
+    )
+    output$downloadPlotDemographics <- downloadHandler(
+      filename = 'Demographics.pdf',
+      content = function(file) {
+        pdf(file = file, width=12, height=8)
+        barChartMosquitoDemographics(IVM_traj)
+        dev.off()
+      })
+    #############################################################################
+  })
+#---#############################################################################
